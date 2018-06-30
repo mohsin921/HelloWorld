4756bcb217316a838128c7e61fb608bbdab5551f
mohsin921 <40692670+mohsin921@users.noreply.github.com>
2018-07-01 00:12:58 +0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -4,9 +4,6 @@ public class hello{
 		System.out.println("Hello world");
 		System.out.println("Hello world");
 		System.out.println("Hello world");
-		System.out.println("Hello world");
-		System.out.println("Hello world");
-		System.out.println("Hello world");
 	}
 
 }
